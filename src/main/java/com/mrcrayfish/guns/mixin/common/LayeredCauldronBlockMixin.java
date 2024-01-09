dec0c45b3e56b405a8789305e0ba111bf8a91dc7
MikhailTapio <mikhailtapio13468@163.com>
2024-01-09 17:53:11 +0800

    Enhancement

--- src/main/java/com/mrcrayfish/guns/mixin/common/LayeredCauldronBlockMixin.java
+++ src/main/java/com/mrcrayfish/guns/mixin/common/LayeredCauldronBlockMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractCauldronBlock;
 import net.minecraft.world.level.block.LayeredCauldronBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,11 +28,15 @@ public abstract class LayeredCauldronBlockMixin extends AbstractCauldronBlock {
     @Inject(method = "entityInside", at = @At("HEAD"))
     private void removeColor(BlockState state, Level level, BlockPos pos, Entity entity, CallbackInfo ci) {
         if (level.isClientSide) return;
+        if (level.getGameTime() % 40 != 0) return; // Reduced frequency of check
         if (!(entity instanceof ItemEntity ie)) return;
         final ItemStack stack = ie.getItem();
         if (!(stack.getItem() instanceof IColored colored)) return;
+        if (!colored.hasColor(stack)) return;
         if (!this.isEntityInsideContent(state, pos, ie)) return;
         colored.removeColor(stack);
-        ie.setItem(stack);
+        final Vec3 p = ie.position();
+        ie.discard();
+        level.addFreshEntity(new ItemEntity(level, p.x, p.y, p.z, stack));
     }
 }
